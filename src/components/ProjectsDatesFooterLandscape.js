d5a6bcfbc74b3ac1b0ab5604620988c1a819412a
Gabtaca <gabrieltaca117@gmail.com>
2026-01-09 12:32:04 -0500

    feat: disable pointer events for ProjectsDatesFooterLandscape and date groups

--- src/components/ProjectsDatesFooterLandscape.js
+++ src/components/ProjectsDatesFooterLandscape.js
@@ -36,6 +36,7 @@ export default function ProjectsDatesFooterLandscape({ projectsData, buttonPosit
         display: "flex",
         flexDirection: "column",
         justifyContent: "flex-start",
+        pointerEvents: "none",
       }}
     >
       {Object.entries(groupedDates).map(([date, buttonIndices], groupIndex) => {
@@ -67,6 +68,7 @@ export default function ProjectsDatesFooterLandscape({ projectsData, buttonPosit
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
+                pointerEvents: "none",
               }}
             >
               {/* Ligne avec des bordures spécifiques */}
